c84584b33bcd9d3dd0b720157279b64e432b2a18
Sanket Rambhad <sanket.rambhad26@gmail.com>
2022-11-13 07:06:48 +0530

    Negative Scenarios

--- src/testscripts/LoginTest.java
+++ src/testscripts/LoginTest.java
@@ -7,21 +7,41 @@ import base.PredefinedActions;
 import pages.LoginPage;
 
 public class LoginTest {
-	
+
 	@Test
 	public void tc1() {
 		System.out.println("STEP - Launch Chrome Browser & Hit url");
-		PredefinedActions.start("srambhad-trials77.orangehrmlive.com");
+		PredefinedActions.start("https://srambhad-trials77.orangehrmlive.com");
 
 		System.out.println("STEP - Enter valid login credentials");
 		LoginPage loginPage = new LoginPage();
 		loginPage.login("Admin", "z6FRB7Ure@");
 
-		System.out.println("VERIFY - home page is displayed");
+		System.out.println("VERIFY - Home page is displayed");
 		String expetedTitle = "Employee Management";
 		String actualTitle = loginPage.getPageTitle();
 
-		Assert.assertEquals(actualTitle, expetedTitle,"Expected title was " + expetedTitle + " but actual title was " + actualTitle);
+		Assert.assertEquals(actualTitle, expetedTitle,
+				"Expected title was " + expetedTitle + " but actual title was " + actualTitle);
+
+		PredefinedActions.closeBrowser();
+	}
+
+	@Test
+	public void tc1_Negative_Secenario() {
+		System.out.println("STEP - Launch Chrome Browser & Hit url");
+		PredefinedActions.start("https://srambhad-trials77.orangehrmlive.com");
+
+		System.out.println("STEP - Enter valid login credentials");
+		LoginPage loginPage = new LoginPage();
+		loginPage.login("Admin", "z6FRB7Ure@123");
+
+		System.out.println("VERIFY - Home page is displayed");
+		String expetedTitle = "OrangeHRM";
+		String actualTitle = loginPage.getPageTitle();
+
+		Assert.assertEquals(actualTitle, expetedTitle,
+				"Expected title was " + expetedTitle + " but actual title was " + actualTitle);
 
 		PredefinedActions.closeBrowser();
 	}
